20995e834934f0f60e840dae79128e2acc62df0e
ChengS1r <jingchenggao.nju@gmail.com>
2021-05-12 10:37:42 +0800

    学习使用文档注释

---
+++ src/DocComments.java
@@ -0,0 +1,17 @@
+/**
+ * @author ChengSir<br>
+ * 2021/5/12
+ * @version jdk 1.8
+ */
+
+public class DocComments {
+    /**
+     * Take the sum of two numbers
+     * @param num1 The first num
+     * @param num2 The second num
+     * @return The sum of num1 and num2
+     */
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+}
