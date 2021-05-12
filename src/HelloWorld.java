7ae37171ccfbaece81ca9da612ac9db5e90a7af8
ChengS1r <jingchenggao.nju@gmail.com>
2021-05-12 10:37:27 +0800

    The first Java code

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World\n");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println();
+    }
+}
